f11cce6d326ff4c56389e8576c980657e4d6c7c1
Xi Han <xihan94@ucla.edu>
2021-01-28 18:13:51 -0800

    Update rules.mk

--- I_GGX64/rules.mk
+++ I_GGX64/rules.mk
@@ -2,3 +2,8 @@ PREFIX := I_GGX64
 
 objs += I_GGX64/main.o
 objs += I_GGX64/uci.o
+objs += I_GGX64/application.o
+objs += I_GGX64/thread.o
+objs += I_GGX64/search.o
+objs += I_GGX64/ucioption.o
+objs += I_GGX64/benchmark.o
