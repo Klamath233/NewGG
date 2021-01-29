2a57c591e548f3e9f63de941e93e34aec0e6cb3b
Xi Han <xihan94@ucla.edu>
2021-01-28 19:18:36 -0800

    Update rules.mk

--- I_GGX64/rules.mk
+++ I_GGX64/rules.mk
@@ -4,7 +4,6 @@ objs+= $(PREFIX)/pos_move.o
 objs+= $(PREFIX)/Um128_find_mul.o
 objs+= $(PREFIX)/move.o
 objs+= $(PREFIX)/pos_mate.o
-objs+= $(PREFIX)/Is_repeat.o
 objs+= $(PREFIX)/control.o
 objs+= $(PREFIX)/move_undo_test.o
 objs+= $(PREFIX)/main.o
